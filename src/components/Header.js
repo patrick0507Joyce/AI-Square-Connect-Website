69e3f2d3dc42deee7a3e57070284539cc6f404c2
Xiaohu Shu <sxh20150609@gmail.com>
2021-01-13 14:51:16 +0200

    dropdown bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 import styled from 'styled-components'
 import { FaBars } from "react-icons/fa"
@@ -8,21 +8,51 @@ import Icon from "../assets/svg/logo_favicon.svg"
 
 const Header = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
-
-  //TODO: navigate to our page subcomponent
+  const [style, setStyle] = useState({display: 'none'});
+  const [menuIndex, setMenuIndex] =useState(-1);
   
   return (
     <Nav>
       <NavLink to="/">
         <Logo src={Icon} alt="Logo"/>
       </NavLink>
-      <Bars onClick={() => setNavbarOpen(!navbarOpen)} />
-      <NavMenu>
+      <Bars onClick={() => setNavbarOpen(true)} />
+      <NavMenu onMouseLeave={e => {
+                     setStyle({display: 'none'});
+                     setMenuIndex(-1);
+                 }}>
+        <NavMenuWrapper>
         {menuData.map((item, index) => (
-          <NavLink to={item.link} key={index}>
-            {item.title}
-          </NavLink>
+          
+            <NavLink to={item.link} key={index} onMouseEnter={e => {
+                     item.subMenus === undefined
+                     ? 
+                     setStyle({display: 'none'}) 
+                     : 
+                     setStyle({});
+                     item.subMenus === undefined
+                     ?
+                     setMenuIndex(-1)
+                     :
+                     setMenuIndex(index);
+                 }}>
+              {item.title}
+            </NavLink>
+
         ))}
+        </NavMenuWrapper>
+        <NavMenuWrapper>
+          {menuData.map((item, index) =>(
+                item.subMenus ? 
+              (<DropDownContent isDropped={index== menuIndex ? true : false}>
+                {item.subMenus.map((subitem, index) => (
+                  <DropDownLink to="/" key={index}>{subitem.title}</DropDownLink>
+                ))}
+              </DropDownContent>) 
+              : 
+              <DropDownContent />
+              ))}
+        </NavMenuWrapper>
       </NavMenu>
       <NavContent navbarOpen={navbarOpen}>
         {menuData.map((item, index) => (
@@ -59,7 +89,6 @@ const NavLink = styled(Link)`
   align-items: center;
   text-decoration: none;
   padding: 0px 1rem;
-  height: 100%;
   cursor: pointer;
   font-size: 1.2rem;
   transition: filter 300ms;
@@ -110,9 +139,10 @@ display: none;
 
 const NavMenu = styled.div `
   display: flex;
+  flex-direction: column;
   align-items: center;
+  margin: 2rem 0;
   
-
   @media screen and (max-width: 768px) {
     display: none;
   }
@@ -136,7 +166,7 @@ const NavContent = styled.div `
   justify-content: center;
   align-items: center;
   text-align: center;
-  height: 50vh;
+  height: 20rem;
   width: 100%;
   position: absolute;
   top: 4rem;
@@ -146,4 +176,54 @@ const NavContent = styled.div `
   @media screen and (min-width: 768px) {
     display: none;
   }
+`
+
+const DropDownContent = styled.div `
+  background-color: rgba(0, 0, 0, 1);
+  display: flex;
+  opacity: ${({isDropped}) => (isDropped ? 1 : 0)};
+  
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: rem;
+
+  li {
+    color: white;
+  }
+`
+const NavMenuWrapper = styled.div `
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  padding: 0.5rem 0;
+`
+
+const DropDownLink = styled(Link)`
+  color: #f7f5f5;
+  filter: brightness(90%);
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 0.5rem;
+  height: 100%;
+  cursor: pointer;
+  font-size: 1.2rem;
+  transition: filter 300ms;
+  margin-top: 1rem;
+
+  &:hover {
+    filter: brightness(200%);
+    color: white;
+    text-decoration: underline;
+    text-decoration-color: #0373FC;
+    text-decoration-thickness: 4px;
+    text-underline-offset: 0.5rem;
+  }
+  
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+
 `
--- src/data/MenuData.js
+++ src/data/MenuData.js
@@ -1,8 +1,32 @@
 export const menuData = [
-    {title: "Company", link: "/company"},
-    {title: "Technology", link: "/technology"},
-    {title: "Benefit", link: "/benefit"},
-    {title: "News", link: "/news"},
-    {title: "R&D", link: "/rd"},
-    {title: "Contact", link: "/contact"}
+  { title: "Company", link: "/company", 
+  subMenus:[
+      {title:"Context",subLink:"company-context"},
+      {title:"Vision",subLink:"company-vision"},
+      {title:"Story",subLink:"company-story"},
+      {title:"Founders",subLink:"company-founder"},
+      {title:"Partners",subLink:"company-partner"},
+    ] 
+  },
+  { title: "Technology", link: "/technology",
+  subMenus:[
+    {title:"Platform",subLink:"technology-platform"},
+    {title:"Description",subLink:"technology-description"},
+    {title:"Supervised",subLink:"technology-supervised"},
+    {title:"Optimisation",subLink:"technology-optimisation"},
+    {title:"Reinforcement",subLink:"technology-reinforcement"},
+  ] 
+ },
+  { title: "Benefit", link: "/benefit",
+  subMenus:[
+    {title:"Strategies",subLink:"benefit-strategies"},
+    {title:"Prediction",subLink:"benefit-prediction"},
+    {title:"Allocation",subLink:"benefit-allocation"},
+    {title:"Crisis",subLink:"benefit-crisis"},
+    {title:"Risk",subLink:"benefit-risk"},
+  ] 
+  },
+  { title: "News", link: "/news" },
+  { title: "R&D", link: "/rd" },
+  { title: "Contact", link: "/contact" },
 ]
